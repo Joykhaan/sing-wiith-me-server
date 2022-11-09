2e64b2143961c1446c51c8d12b8ce12cfd79c62c
Joykhaan <perfectkhan421@gmail.com>
2022-11-09 16:57:21 +0600

    db setup cmplete

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+// middle ware
 app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASSWORD}@cluster0.wfqwiph.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+// signwithjoy
+// V1b71GllqbrI1uzO
+// console.log(process.env.DB_USER)
 app.get('/',(req,res)=>{
     res.send('look mama tor heda')
 });
